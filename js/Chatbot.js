c1ad8d91911735a52eae31ee10dcc9ecaf6579fc
sapna770 <sapnakumawat303.com>
2022-03-06 11:32:48 +0530

    Updates

---
+++ js/Chatbot.js
@@ -0,0 +1,9 @@
+var Tawk_API = Tawk_API || {}, Tawk_LoadStart = new Date();
+(function () {
+    var s1 = document.createElement("script"), s0 = document.getElementsByTagName("script")[0];
+    s1.async = true;
+    s1.src = 'https://embed.tawk.to/6215df6f1ffac05b1d7b4347/1fsin1jp5';
+    s1.charset = 'UTF-8';
+    s1.setAttribute('crossorigin', '*');
+    s0.parentNode.insertBefore(s1, s0);
+})();
